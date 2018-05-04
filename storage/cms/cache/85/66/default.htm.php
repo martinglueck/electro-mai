df58c60eabe8f3f2a895219c6b96f5f2cdb0b2d5
martinglueck <mar.glueck@web.de>
2018-05-04 22:10:14 +0200

    clear views

--- storage/cms/cache/85/66/default.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5aecaabac99e4326699514_81510aeec35c720057f47dfc6ed19109Class extends \Cms\Classes\LayoutCode
-{
-
-}
--- storage/cms/cache/a8/8d/footer.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5aecaabae3be3192376287_91499e90df69808b70ba908b76314ba2Class extends Cms\Classes\PartialCode
-{
-
-}
